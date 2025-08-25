9ddf27bb35fc53de78016ff28b4e098fdf6f359b
reubingeorge <47335699+reubingeorge@users.noreply.github.com>
2025-08-25 15:32:08 -0400

    render changes added for deployment

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const morgan = require('morgan');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 
 // Middleware
 app.use(helmet());
